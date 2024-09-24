dff184caa26ff50e771a3252c807162eb58d5505
6124436 <6124436@TR-HRCTDX3>
2024-09-24 11:46:11 +0530

    Add project files.

---
+++ MedicalRecordAutomation/LoginUITest.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THomsonReuters.MedicalRecordAutomation
+{
+    public class LoginUITest
+    {
+        [Test]
+        public void TitleTest()
+        { 
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            driver.Navigate().GoToUrl("https://demo.openemr.io/b/openemr");
+
+            string actualTitle = driver.Title;
+            Assert.That(actualTitle, Is.EqualTo("OpenEMR Login"));
+        }
+
+        [Test]
+        public void ApplicationDescriptionTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            driver.Navigate().GoToUrl("https://demo.openemr.io/b/openemr");
+
+            //Assert the text --> "The most popular open-source Electronic Health Record and Medical Practice Management solution."
+            string actualDescription = driver.FindElement(By.XPath("//p[contains(text(),'most')]")).Text;
+            Assert.That(actualDescription, Is.EqualTo("The most popular open-source Electronic Health Record and Medical Practice Management solution."));
+        }
+}
